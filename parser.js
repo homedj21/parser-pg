631249e951f647e4540b4ab50ee41549cfc4d72f
homedj21 <homedj21@gmail.com>
2016-12-09 18:04:49 +0400

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,31 +1,27 @@
-//Подключаем модули
-var request = require('request'), cheerio = require('cheerio'), iconv = require('iconv-lite'), mysql = require('mysql');
-//Параметры подключения к базе данных
-var client = mysql.createConnection({
-user: 'root',
-password: 'root',
-database: 'db'
-});
-//Очищаем таблицу
-client.query('TRUNCATE TABLE apps');
-//Загружаем страницу
-var i;
-for (i = 0; i <= 480; i=i+60) {
-request({uri:'https://play.google.com/store/apps/collection/topselling_free?start='+i, method:'GET', encoding:'binary'},
-        function (err, res, page) {
-//Передаём страницу cheerio
-        var $=cheerio.load(iconv.encode(new Buffer (page, 'binary'), 'utf8'));
-//Находим нужные CSS-селекторы
-        $('a.title').each(function() {
-			src=$(this).attr("href");
-			title=$(this).attr("title");
-//Вывод в консоль
-        console.log(title + ' - ' + 'https://play.google.com' + src);
-        var srcnew='https://play.google.com' + src;
-//Вставляем данные в базу
-client.query('INSERT INTO `apps` (`title`, `src`) VALUES (?, ?)', [title, srcnew]);
-
-    });
-	
-	});
-}
+var http = require('http'), request = require('request'), cheerio = require('cheerio'), iconv = require('iconv-lite');                 
+var i;                                                                                                                                 
+var arr=[];                                                                                                                            
+arr["apps"]=[];                                                                                                                        
+var n=1;                                                                                                                               
+                                                                                                                                       
+for (i = 0; i <= 480; i=i+60) {                                                                                                        
+request({uri:'https://play.google.com/store/apps/collection/topselling_free?start='+i, method:'GET', encoding:'binary'},               
+        function (err, res, page) {                                                                                                    
+        var $=cheerio.load(iconv.encode(new Buffer (page, 'binary'), 'utf8'));                                                         
+        $('a.title').each(function() {                                                                                                 
+                        src=$(this).attr("href");                                                                                      
+                        title=$(this).attr("title");                                                                                   
+                                                                                                                                       
+                                                                                                                                       
+arr.apps.push('\n' + n + '. ' + title + ' - ' + 'https://play.google.com' + src);                                                      
+n=n+1;                                                                                                                                 
+    });                                                                                                                                
+                                                                                                                                       
+        });                                                                                                                            
+}                                                                                                                                      
+                                                                                                                                       
+http.createServer(function(request, response) {                                                                                        
+response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});                                                                
+response.write(' '+arr.apps.toString());                                                                                               
+response.end();                                                                                                                        
+}).listen(81)                                                                                                                          
