417ad26c14718d0743bf33b44ca4e68a1e4fa32b
homedj21 <homedj21@gmail.com>
2016-12-12 18:52:08 +0400

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,7 +1,6 @@
 var http = require('http'), request = require('request'), cheerio = require('cheerio'), iconv = require('iconv-lite');                 
 var i;                                                                                                                                 
-var arr=[];                                                                                                                            
-arr["apps"]=[];                                                                                                                        
+var arr=[];                                                                                                                                                                                                                                                  
 var n=1;                                                                                                                               
                                                                                                                                        
 for (i = 0; i <= 480; i=i+60) {                                                                                                        
@@ -13,15 +12,20 @@ request({uri:'https://play.google.com/store/apps/collection/topselling_free?star
                         title=$(this).attr("title");                                                                                   
                                                                                                                                        
                                                                                                                                        
-arr.apps.push('\n' + n + '. ' + title + ' - ' + 'https://play.google.com' + src);                                                      
+arr.push('\n' + n + '. ' + title + ' - ' + 'https://play.google.com' + src);                                                      
 n=n+1;                                                                                                                                 
     });                                                                                                                                
                                                                                                                                        
         });                                                                                                                            
 }                                                                                                                                      
-                                                                                                                                       
+   
+
+
+function Vyvod(arg) {
 http.createServer(function(request, response) {                                                                                        
 response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});                                                                
-response.write(' '+arr.apps.toString());                                                                                               
+response.write(arr.toString());                                                                                               
 response.end();                                                                                                                        
 }).listen(81)                                                                                                                          
+}
+Vyvod(arr);
