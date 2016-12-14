ac836e2b8fb1246896c2100b7ca0b72edab60827
homedj21 <homedj21@gmail.com>
2016-12-14 13:20:12 +0400

    Update parser.js

--- parser.js
+++ parser.js
@@ -1,31 +1,42 @@
-var http = require('http'), request = require('request'), cheerio = require('cheerio'), iconv = require('iconv-lite');                 
-var i;                                                                                                                                 
-var arr=[];                                                                                                                                                                                                                                                  
-var n=1;                                                                                                                               
-                                                                                                                                       
-for (i = 0; i <= 480; i=i+60) {                                                                                                        
-request({uri:'https://play.google.com/store/apps/collection/topselling_free?start='+i, method:'GET', encoding:'binary'},               
-        function (err, res, page) {                                                                                                    
-        var $=cheerio.load(iconv.encode(new Buffer (page, 'binary'), 'utf8'));                                                         
-        $('a.title').each(function() {                                                                                                 
-                        src=$(this).attr("href");                                                                                      
-                        title=$(this).attr("title");                                                                                   
-                                                                                                                                       
-                                                                                                                                       
-arr.push('\n' + n + '. ' + title + ' - ' + 'https://play.google.com' + src);                                                      
-n=n+1;                                                                                                                                 
-    });                                                                                                                                
-                                                                                                                                       
-        });                                                                                                                            
-}                                                                                                                                      
-   
+request = require('request'), cheerio = require('cheerio'), iconv = require('iconv-lite');
 
+function  Sbor(){
+var i;
+var array=[];
+var num=1;
+for (i = 0; i <= 480; i=i+60) {
+request ({
+           uri:'https://play.google.com/store/apps/collection/topselling_free?start='+i,
+           method:'GET',
+           encoding:'binary'
+                             },
+             function (err, res, page) {
+             var $=cheerio.load(iconv.encode(new Buffer (page, 'binary'), 'utf8'));
+             $('a.title').each(function() {
+                        link=$(this).attr("href");
+                        title=$(this).attr("title");
+                        array.push( '\n' +  num + '. ' + title + ' - ' + 'https://play.google.com' + link  );
+                        num=num+1;
+                 });
 
-function Vyvod(arg) {
-http.createServer(function(request, response) {                                                                                        
-response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});                                                                
-response.write(arr.toString());                                                                                               
-response.end();                                                                                                                        
-}).listen(81)                                                                                                                          
+          });
+
+    }
+
+return array;
+
 }
-Vyvod(arr);
+
+
+function Vyvod() {
+
+var http = require('http');
+var list = Sbor();
+http.createServer(function(request, response) {
+response.writeHead(200, {"Content-Type": "text/plain; charset=utf-8"});
+response.write(list.toString());
+response.end();
+}).listen(81)
+}
+
+var vyvod = new Vyvod();
